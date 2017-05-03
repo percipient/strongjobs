98da768f82468e3dc598ee25af051d511d88604e
Patrick Cloke <clokep@patrick.cloke.us>
2017-05-03 15:43:14 -0400

    Fix bug with current version.

--- jobs/update-dependencies/update-node-dependencies.py
+++ jobs/update-dependencies/update-node-dependencies.py
@@ -48,8 +48,11 @@ def npm_outdated(req_file_path):
     # The first line is the column titles, the last is blank.
     package_updates = {}
     for line in result.split('\n')[1:-1]:
+        # "Current" version is what's installed, "wanted" is what's in the file.
+        # Nowhere does this machine install these dependencies thoug, so
+        # current_version should always be the string "MISSING".
         package, current_version, wanted_version, latest_version = line.split()
-        package_updates[package] = (current_version, latest_version)
+        package_updates[package] = (wanted_version, latest_version)
 
     return package_updates
 
